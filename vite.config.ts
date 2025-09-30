b0b2f5528026ed91853be6192f725993a96af625
expresssorder-spec <expresss.order@gmail.com>
2025-09-30 05:03:33 +0100

    Update base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This base path is for GitHub Pages deployment.
-  // It should match your repository name.
   base: '/Car/',
 })
